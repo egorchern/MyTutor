f0923f6a22834d042acf0845c9409220a1005243
Egor Chernyshev <egorch.formal@gmail.com>
2020-09-14 23:04:09 +0100

    added slideshow functionality

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $(function(){
     $('#wrapper').bind("scroll", function(e){
         scrl();
     });
+    load_slideshow("my-slide-show");
+
 })
 
 
@@ -22,4 +24,40 @@ function scrl(){
         }
         
     }
+}
+let slide_show_index = 0;
+let image_sources = ["Assets/feedback_1.PNG"];
+function load_slideshow(id){
+    $(`#${id} > .slide-show-image-container > img`).attr("src", image_sources[slide_show_index]);
+    for (let i = 0; i < image_sources.length;i++){
+        $(`#${id} > .slide-show-dots-container`).append(`
+        <div class='slide-show-dot' data-index='${i}'>
+        </div>
+        `)
+    }
+    $(`#${id} > .slide-show-dots-container > .slide-show-dot:first-child`).addClass("slide-show-dot-active");
+    $(`#${id} .right-slide-btn`).bind("click", function(e){
+        if(slide_show_index < image_sources.length - 1){
+            slide_show_index += 1;
+            $(`#${id} > .slide-show-image-container > img`).attr("src", image_sources[slide_show_index]);
+            $(`#${id} > .slide-show-dots-container > .slide-show-dot`).removeClass("slide-show-dot-active");
+            $(`#${id} > .slide-show-dots-container > .slide-show-dot:nth-child(${slide_show_index + 1})`).addClass("slide-show-dot-active");
+        }
+    });
+    $(`#${id} .left-slide-btn`).bind("click", function(e){
+        if(slide_show_index > 0){
+            slide_show_index -= 1;
+            $(`#${id} > .slide-show-image-container > img`).attr("src", image_sources[slide_show_index]);
+            $(`#${id} > .slide-show-dots-container > .slide-show-dot`).removeClass("slide-show-dot-active");
+            $(`#${id} > .slide-show-dots-container > .slide-show-dot:nth-child(${slide_show_index + 1})`).addClass("slide-show-dot-active");
+        }
+    });
+    $(`#${id} > .slide-show-dots-container > .slide-show-dot`).bind("click", function(e){
+        let index = Number(this.dataset.index);
+        slide_show_index = index;
+        $(`#${id} > .slide-show-image-container > img`).attr("src", image_sources[slide_show_index]);
+        $(`#${id} > .slide-show-dots-container > .slide-show-dot`).removeClass("slide-show-dot-active");
+        $(`#${id} > .slide-show-dots-container > .slide-show-dot:nth-child(${slide_show_index + 1})`).addClass("slide-show-dot-active");
+    })
+
 }
